73d211bb6348d6b760ae326c136e01230c14ce8a
Nathan Zimmerman <npzimmerman@gmail.com>
2014-11-20 12:25:59 -0500

    Configuration module to govern other modules' behavior

---
+++ static/js/config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function(mos) {
+    mos.config = {};
+    mos.config.barchart = {
+        domId: "svg#barchart"
+    }; // TODO: DRY out the code by using this module more
+
+}(MOS));
+
