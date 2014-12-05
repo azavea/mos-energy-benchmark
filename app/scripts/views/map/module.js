c43bd7051f9afd4fbecf9beb82f425618c6ab09c
Kathryn Killebrew <kkillebrew@azavea.com>
2014-12-05 15:29:34 -0500

    Add map using leaflet directive

---
+++ app/scripts/views/map/module.js
@@ -0,0 +1,27 @@
+(function () {
+'use strict';
+
+    /**
+     * @ngInject
+     */
+    function StateConfig($stateProvider) {
+        $stateProvider.state('map', {
+            url: '/map',
+            templateUrl: 'scripts/views/map/map-partial.html',
+            controller: 'MapController'
+        });
+    }
+
+    /**
+     * @ngdoc overview
+     * @name mos.views
+     * @description
+     * # mos
+     */
+    angular
+      .module('mos.views.map', [
+        'ui.router',
+        'leaflet-directive'
+      ]).config(StateConfig);
+
+})();
