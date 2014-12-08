f3fc1fe751915f8bccef50555f3fc7cce190cc11
Kathryn Killebrew <kkillebrew@azavea.com>
2014-12-08 17:23:55 -0500

    Add loading animations for map and popups

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 BuildingEnergyBenchmarking
 ==========================
 
-This [project](https://azavea.github.io/mos-energy-benchmark/ "Energy Benchmarking") is hosted by github. For development, just run `serve.sh` and head on over to port 9000 on localhost.
+This [project](https://azavea.github.io/mos-energy-benchmark/ "Energy Benchmarking") is hosted by github.
 
 ### Developing
 
--- app/scripts/views/map/map-controller.js
+++ app/scripts/views/map/map-controller.js
@@ -11,19 +11,19 @@
         // initialization
         $scope.compare_selections = [];  // array of up to three selections to compare
         $scope.haveThree = false;
+        $scope.popupLoading = true;
 
-        // TODO: indicate that map is loading, hang on...
-        console.log('in map controller; going to load map viz...');
+        // indicate that map is loading, hang on...
+        $scope.mapLoading = true;
 
         $scope.compare = function() {
-            console.log('go compare ' + $scope.cartodb_id);
             if ($scope.haveThree) {
                 // shouldn't get here (button is disabled)
                 console.error('Cannot compare more than three items at a time');
             } else {
                 // check to see if we have this one already
                 if ($scope.compare_selections.indexOf($scope.cartodb_id) > -1) {
-                    console.log('already have ' + $scope.cartodb_id + ' in comparison list');
+                    console.error('already have ' + $scope.cartodb_id + ' in comparison list');
                 } else {
                     $scope.compare_selections.push($scope.cartodb_id);
                     $scope.haveThree = $scope.compare_selections.length >=3 ? true : false;
@@ -32,20 +32,31 @@
         };
 
         var popup_template = '<span>' +
-              '<h4>Address:</h4>' +
+              '<div ng-show="popupLoading" class="spinner">' +
+              '<div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div>' +
+              '</div>' +
+              '<div ng-hide="popupLoading"><h4>Address:</h4>' +
               '<p>{{address}}</p>' +
               '<h4>Emissions:</h4>' +
               '<p>{{total_ghg}}</p>' +
               '<h4>ID:</h4>' + 
               '<p>{{cartodb_id}}</p>' +
               '<p><button ng-click="compare()" ng-disabled="haveThree">Compare</button></p>' +
-              '</span>';
+              '</div></span>';
+
+        var showPopup = function(map, coords) {
+            var popup = $compile(popup_template)($scope);
+            $scope.$apply(); // tell Angular to really, really go compile now
+            L.popup({
+                minWidth: 100
+            }).setLatLng(coords).setContent(popup[0]).openOn(map);
+        };
 
         // load map visualization
         cartodb.createVis('mymap', 'http://azavea-demo.cartodb.com/api/v2/viz/c5a9af6e-7f12-11e4-8f24-0e018d66dc29/viz.json',
                           {'infowindow': false})
             .done(function(vis, layers) {
-                console.log('map viz loaded!');
+                $scope.mapLoading = false;
                 var nativeMap = vis.getNativeMap();
                 var overlay = layers[1];
 
@@ -66,9 +77,10 @@
 
                 sublayer.on('featureClick', function(e, latlng, pos, data, subLayerIndex) {
 
-                    // TODO: something to indicate popup is loading, hang on...
-                    console.log('hey, that tickles!');
-
+                    // show popup with spinner to indicate it's loading, hang on...
+                    $scope.popupLoading = true;
+                    showPopup(nativeMap, latlng);
+                    
                     // Go fetch data for this feature; 'data' object from click method only has
                     // the interactivity field, which is the cartodb_id.
                     var qry = 'SELECT cartodb_id, geocode_address, total_ghg, property_name ' + 
@@ -81,16 +93,13 @@
                             $scope.address = data.geocode_address;
                             $scope.total_ghg = data.total_ghg;
 
-                            var popup = $compile(popup_template)($scope);
-                            $scope.$apply(); // tell Angular to really, really go compile now
-
-                            L.popup({
-                                minWidth: 100
-                            }).setLatLng(latlng).setContent(popup[0]).openOn(nativeMap);
+                            // update popup now we have the data
+                            $scope.popupLoading = false;
+                            showPopup(nativeMap, latlng);
 
                         }).error(function(errors) {
                             // returns a list
-                            console.log('errors: ' + errors);
+                            console.error('errors: ' + errors);
                         });
                 });
             });
