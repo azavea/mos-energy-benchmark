8149b3a68770ad622125f2be0621dc95d73f5eb3
CloudNiner <andrew.fink.11@gmail.com>
2014-12-09 11:47:24 -0500

    Add data fields to detail page

--- app/scripts/views/detail/detail-controller.js
+++ app/scripts/views/detail/detail-controller.js
@@ -4,11 +4,32 @@
     /*
      * ngInject
      */
-    function DetailController(buildingData) {
-        console.log('detail: ', buildingData.data);
+    var DetailConfig = {
+        fields: {
+            'electricity': 'Electricity',
+            'site_eui': 'EUI',
+            'total_ghg': 'Greenhouse Emissions',
+            'water_use': 'Water Use',
+            'natural_gas': 'Natural Gas'
+        }
+    };
+
+    /*
+     * ngInject
+     */
+    function DetailController($scope, DetailConfig, buildingData) {
+
+/* jshint laxbreak:true */
+        var building = buildingData.data && buildingData.data.rows && buildingData.data.rows.length > 0
+                        ? buildingData.data.rows[0] : {};
+/* jshint laxbreak:false */
+        console.log(building);
+        $scope.building = building;
+        $scope.fields = DetailConfig.fields;
     }
 
     angular.module('mos.views.detail')
+    .constant('DetailConfig', DetailConfig)
     .controller('DetailController', DetailController);
 
 })();
