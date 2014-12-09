8134d17e936c0d73ae0f2735d5a21201ec0c6fa0
Nathan Zimmerman <npzimmerman@gmail.com>
2014-12-09 15:44:39 -0500

    Got the barchart almost entirely functional - needs a second pair of eyes for the bug I'm encountering

--- app/scripts/charting/barchart-directive-sqft.js
+++ app/scripts/charting/barchart-directive-sqft.js
@@ -5,32 +5,37 @@
     var BarChartDefaultsSqft = {
         plotWidth: 800,
         plotHeight: 200,
-        yLabel: 'energystar',
-        xLabel: 'avgsqfeet',
+        yDefault: 'energystar',
+        xDefault: 'avgsqfeet',
+        transitionMillis: 500,
     };
 
     /**
      * ngInject
      */
-    function barChartSqft (BarChartDefaultsSqft) {
+    function barChartSqft (CartoConfig, BarChartDefaultsSqft) {
 
         var PLOT_CLASS = 'mos-barchart';
-
-        // Private vars
-        var leftAxisG = null;
-        var bottomAxisG = null;
 
         // Begin directive module definition
         var module = {};
 
         module.restrict = 'EA';
-        module.template = '<svg class="chart"></svg>';
+        module.template = [
+            '<svg class="chart"></svg>',
+            '<div class="controls">',
+            '  <div class="axis y">',
+            '    Y axis',
+            '    <select ng-model="selectedY" ng-options="key as value for (key, value) in selectOptions" ng-change="selectedYChanged()"></select>',
+            '  </div>',
+            '</div>'
+        ].join('');
         module.controller = 'ChartingController';
 
         module.scope = {
             data: '=',              // Required
             id: '@',                // Required
-            xLabel: '@',              // Required
+            xDefault: '@',              // Required
             plotWidth: '@',
             plotHeight: '@',
             margin: '&',
@@ -38,9 +43,15 @@
 
         module.link = function ($scope, element, attrs) {
             $scope.configure(BarChartDefaultsSqft);
-
             var config = $scope.config;
             config.margin.left = 0;
+            $scope.selectOptions = _.merge(_.omit(CartoConfig.labels, 'squarefeet'), {
+                'avgsqfeet': 'Avg Sq. Ft.',
+                'mediansqfeet': 'Median Sq. Ft.',
+            });
+            $scope.selectedY = config.yDefault;
+            var yAttr = $scope.selectedY;
+
 
 
             element.addClass(PLOT_CLASS);
@@ -56,9 +67,8 @@
                 .attr('transform', 'translate(' + config.margin.left + ',' + (config.plotHeight - config.margin.bottom) + ')');
 
 
-            // Overridden ChartingController method
-            $scope.plot = function(data) {
-                // This annoying junk is to logarithmically bin years - we have fewer old buildings
+            // This is a helper function for transforming api data into percentiles based on sqft
+            function transformData(data, groups) {
                 var orderedData = _.sortBy(
                     _.filter(data, function(q) {
                         return q.sqfeet > 0;
@@ -66,45 +76,48 @@
                             return p.sqfeet;
                         });
 
-                var pctiles = function(vals, groupCount) {
-                    var reverseVals = vals.reverse();
-                    var dataCount = vals.length;
-                    var divider = Math.ceil(dataCount/groupCount);
-                    var binnedByPctile = {};
-                    var pctileSize = 100/groupCount;
-                    var pctileHolder;
-                    for (var i = 0; i < groupCount; i++) {
-                        pctileHolder = [];
-                        for (var j = 0; j < divider; j++) {
-                            pctileHolder = pctileHolder.concat(reverseVals.pop());
-                        }
-                        binnedByPctile[pctileSize*i] = pctileHolder;
+                var reverseData = orderedData.reverse();
+                var dataCount = orderedData.length;
+                var divider = Math.ceil(dataCount/groups);
+                var binnedByPctile = {};
+                var pctileSize = 100/groups;
+                var pctileHolder;
+                for (var i = 0; i < groups; i++) {
+                    pctileHolder = [];
+                    for (var j = 0; j < divider; j++) {
+                        pctileHolder = pctileHolder.concat(reverseData.pop());
                     }
+                    binnedByPctile[pctileSize*i] = pctileHolder;
+                }
 
-                    var trashCount = divider-dataCount%divider; // count of undefined in last array
-                    for (i = 0; i < trashCount; i++) {
-                        binnedByPctile[pctileSize*(groupCount-1)].pop();
-                    }
-                    var output = [];
-                    _.forEach(binnedByPctile, function(d, k) {
-                        var kCount = d.length;
-                        output = output.concat({
-                            emissions: _.reduce(d, function(val, memo) { return memo.emissions + val; }, 0) / kCount,
-                            energystar: kCount > 0 ? _.reduce(d, function(val, memo) { return memo.energystar + val; }, 0) / kCount : 0,
-                            eui: kCount > 0 ? _.reduce(d, function(val, memo) { return memo.eui + val; }, 0) / kCount : 0,
-                            minsqfeet: _.min(d, function(val) { return val.sqfeet; }).sqfeet,
-                            maxsqfeet: _.max(d, function(val) { return val.sqfeet; }).sqfeet,
-                            avgsqfeet: _.reduce(d, function(memo, val) { return val.sqfeet + memo; }, 0) / kCount,
-                            mediansqfeet: _.sortBy(d, function(val) { return val.sqfeet; })[Math.round(kCount/2)].sqfeet,
-                            totalsqfeet: _.reduce(d, function( memo, val) { return val.sqfeet + memo; }, 0),
-                            count: kCount,
-                            pctile: k,
-                            yearRange: _.min(d, function(d) { return d.yearbuilt; }).yearbuilt + '-' + _.max(d, function(d) { return d.yearbuilt; }).yearbuilt
-                        });
+                var trashCount = divider-dataCount%divider; // count of undefined in last array
+                for (i = 0; i < trashCount; i++) {
+                    binnedByPctile[pctileSize*(groups-1)].pop();
+                }
+                var output = [];
+                _.forEach(binnedByPctile, function(d, k) {
+                    var kCount = d.length;
+                    output = output.concat({
+                        emissions: _.reduce(d, function(val, memo) { return memo.emissions + val; }, 0) / kCount,
+                        energystar: kCount > 0 ? _.reduce(d, function(val, memo) { return memo.energystar + val; }, 0) / kCount : 0,
+                        eui: kCount > 0 ? _.reduce(d, function(val, memo) { return memo.eui + val; }, 0) / kCount : 0,
+                        minsqfeet: _.min(d, function(val) { return val.sqfeet; }).sqfeet,
+                        maxsqfeet: _.max(d, function(val) { return val.sqfeet; }).sqfeet,
+                        avgsqfeet: _.reduce(d, function(memo, val) { return val.sqfeet + memo; }, 0) / kCount,
+                        mediansqfeet: _.sortBy(d, function(val) { return val.sqfeet; })[Math.round(kCount/2)].sqfeet,
+                        totalsqfeet: _.reduce(d, function( memo, val) { return val.sqfeet + memo; }, 0),
+                        count: kCount,
+                        pctile: k,
+                        yearRange: _.min(d, function(d) { return d.yearbuilt; }).yearbuilt + '-' + _.max(d, function(d) { return d.yearbuilt; }).yearbuilt
                     });
-                    return output.reverse();
-                };
-                data = pctiles(orderedData, 20);
+                });
+                return output.reverse();
+            }
+
+
+            // Overridden ChartingController method
+            $scope.plot = function(data) {
+                data = transformData(data, 20);
 
                 chart.attr('height', config.plotHeight);
 
@@ -116,16 +129,19 @@
                 bottomAxisG.call(bottomAxis);
 
                 var y = d3.scale.linear()
-                    .domain([0, d3.max(data, function(d) { return d[config.yLabel]; })])
+                    .domain([0, d3.max(data, function(d) { return d[yAttr]; })])
                     .range([config.plotHeight-config.margin.bottom-config.margin.top, 0]);
                 leftAxis.scale(y);
-                //leftAxisG.call(leftAxis);
 
                 // Tooltips
                 var tip = d3.tip()
                   .attr('class', 'd3-tip')
                   .offset([-10, 0])
-                  .html(function(d) { return '<span class="propertyName">' + d.pctile+'th percentile</span>'; });
+                  .html(function(d) {
+                      return '<div class="propertyName">' +
+                          d.pctile + 'th percentile</div><div class="propertyName y">\n' +
+                          yAttr + ': ' + Math.round(y(d[yAttr])) + '</div>';
+                  });
                 chart.call(tip);
 
                 // Bars
@@ -133,12 +149,32 @@
                     .data(data)
                     .enter().append('rect')
                     .attr('transform', function(d) { return 'translate(' + (x(d.pctile) + config.margin.left).toString() + ',0)'; })
-                    .attr('y', function(d) {return y(d[config.yLabel]) + config.margin.top; })
-                    .attr('height', function(d) { return config.plotHeight - y(d[config.yLabel]) - config.margin.top - config.margin.bottom; })
+                    .attr('y', function(d) {return y(d[yAttr]) + config.margin.top; })
+                    .attr('height', function(d) { return config.plotHeight - y(d[yAttr]) - config.margin.top - config.margin.bottom; })
                     .attr('width', x.rangeBand())
                     .on('mouseover', tip.show)
                     .on('mouseout', tip.hide);
 
+                // Animation
+                function refreshScale() {
+                    y.domain([0, d3.max(data, function(d) { return d[yAttr]; })]);
+                }
+
+                function refreshData() {
+                    var transitionMillis = config.transitionMillis;
+                    refreshScale();
+
+                    chart.selectAll('rect')
+                        .transition()
+                        .ease('linear')
+                        .duration(transitionMillis)
+                        .attr('y', function(d) {return y(d[yAttr]) + config.margin.top; })
+                        .attr('height', function(d) { return config.plotHeight - y(d[yAttr]) - config.margin.top - config.margin.bottom; });
+                }
+                $scope.selectedYChanged = function() {
+                    yAttr = $scope.selectedY;
+                    refreshData();
+                };
             };
         };
 
--- app/scripts/charting/barchart-directive-temporal.js
+++ app/scripts/charting/barchart-directive-temporal.js
@@ -5,14 +5,13 @@
     var BarChartDefaultsTemporal = {
         plotWidth: 800,
         plotHeight: 200,
-        yLabel: 'emissions',
-        xLabel: 'yearbuilt',
+        yDefault: 'avgemissions',
     };
 
     /**
      * ngInject
      */
-    function barChartTemporal (BarChartDefaultsTemporal) {
+    function barChartTemporal (CartoConfig, BarChartDefaultsTemporal) {
 
         var PLOT_CLASS = 'mos-barchart';
 
@@ -20,7 +19,15 @@
         var module = {};
 
         module.restrict = 'EA';
-        module.template = '<svg class="chart"></svg>';
+        module.template = [
+            '<svg class="chart"></svg>',
+            '<div class="controls">',
+            '  <div class="axis y">',
+            '    Y axis',
+            '    <select ng-model="selectedY" ng-options="key as value for (key, value) in selectOptions" ng-change="selectedYChanged()"></select>',
+            '  </div>',
+            '</div>'
+        ].join('');
         module.controller = 'ChartingController';
 
         module.scope = {
@@ -31,11 +38,43 @@
             margin: '&',
         };
 
+        // Helper function for transforming unshaped data into data binned and aggregated over 5 year periods
+        function transformData(data) {
+            var filteredData = _.filter(data, function(d) { return d.yearbuilt > 1849 ; });
+            var dateRanges = _.zip(_.range(1849, 2015, 5), _.range(1853, 2019, 5));
+            var rolledData =
+                d3.nest()
+                    .key(function(d) {
+                        var years = _.find(dateRanges, function(range) {
+                            return range[0] <= d.yearbuilt && d.yearbuilt <= range[1];
+                        });
+                        return years[0]+'-'+years[1];
+                    })
+                    .rollup(function(d) {
+                        return {
+                            'avgsqft': d3.mean(d, function(e) { return +e.sqfeet; }),
+                            'avgeui': d3.mean(d, function(e) { return +e.eui; }),
+                            'avgemissions': d3.mean(d, function(e) { return +e.emissions; }),
+                            'avgenergystar': d3.mean(d, function(e) { return +e.energystar; }),
+                            'count': d3.sum(d, function() { return +1; })
+                        };
+                    })
+                    .entries(filteredData);
+            return _.sortBy(rolledData, function(d) { return d.key; }).reverse();
+        }
+
         module.link = function ($scope, element, attrs) {
             $scope.configure(BarChartDefaultsTemporal);
-
             var config = $scope.config;
             config.margin.left = 0;
+            $scope.selectOptions = {
+                'avgsqft': 'Avg Sq. Ft.',
+                'avgeui': 'Avg EUI',
+                'avgemissions': 'Avg Emissions',
+                'avgenergystar': 'Avg Energystar',
+            };
+            $scope.selectedY = config.yDefault;
+            var yAttr = $scope.selectedY;
 
 
             element.addClass(PLOT_CLASS);
@@ -50,89 +89,63 @@
                 .attr('class', 'x axis')
                 .attr('transform', 'translate(' + config.margin.left + ',' + (config.plotHeight - config.margin.bottom) + ')');
 
-
             // Overridden ChartingController method
             $scope.plot = function(data) {
-                console.log(data);
-                console.log(config.xLabel)
-                // This annoying junk is to logarithmically bin years - we have fewer old buildings
-                var nestedByLog = d3.nest()
-                    .key(function(d) {
-                        return (2014 - d[config.xLabel]);
-                    })
-                    .rollup(function(d) {
-                        return {
-                            emissions: d3.sum(d, function(g) { return +g.emissions; }),
-                            energystar: d3.sum(d, function(g) { return +g.energystar; }),
-                            eui: d3.sum(d, function(g) { return +g.eui; }),
-                            sqfeet: d3.sum(d, function(g) { return +g.sqfeet; }),
-                            count: d3.sum(d, function() { return +1; }),
-                            yearbuilt: d[0].yearbuilt
-                        };
-                    })
-                    .entries(data);
-                console.log(nestedByLog);
-                var groupUp = function(vals, groupCount) {
-                    var maxVal = _.max(_.map(vals, function(d) { return d.key; }));
-                    var divider = maxVal/groupCount;
-                    var binstarts = d3.range(0, maxVal, divider);
-                    var binnedByLog = {};
-                    for (var i = 0; i < binstarts.length - 1; i++) {
-                        binnedByLog[binstarts[i]] = _.filter(vals, function(d) {
-                            return binstarts[i] <= parseFloat(d.key) && parseFloat(d.key) < binstarts[i+1];
-                        });
-                    }
-                    var output = [];
-                    _.forEach(binnedByLog, function(v, k) {
-                        var kCount = _.reduce(v, function(val, memo) {return memo.values.count + val; }, 0);
-                        output = output.concat({
-                            key: k,
-                            emissions: _.reduce(v, function(val, memo) { return memo.values.emissions + val; }, 0) / kCount,
-                            energystar: v.length > 0 ? _.reduce(v, function(val, memo) { return memo.values.energystar + val; }, 0) / kCount : 0,
-                            eui: v.length > 0 ? _.reduce(v, function(val, memo) { return memo.values.eui + val; }, 0) / kCount : 0,
-                            sqfeet: _.reduce(v, function(val, memo) { return memo.values.sqfeet + val; }, 0) / kCount,
-                            count: kCount,
-                            yearRange: v.length > 0 ? _.min(v, function(d) { return d.values.yearbuilt; }).values.yearbuilt + '-' + _.max(v, function(d) { return d.values.yearbuilt; }).values.yearbuilt : null,
-                        });
-                    });
-                    return _.filter(output, function(d) { return d.yearRange !== null; });
-                };
-                data = groupUp(nestedByLog, 25);
-                console.log(data);
+                data = transformData(data);
 
                 chart.attr('height', config.plotHeight);
 
                 // Axes
                 var x = d3.scale.ordinal()
-                    .domain(data.map(function(d) { return d.yearRange; }))
+                    .domain(data.map(function(d) { return d.key; }))
                     .rangeRoundBands([config.plotWidth-config.margin.left-config.margin.right, 0], 0.05);
                 bottomAxis.scale(x);
                 bottomAxisG.call(bottomAxis);
 
                 var y = d3.scale.linear()
-                    .domain([0, d3.max(data, function(d) { return d[config.yLabel]; })])
+                    .domain([0, d3.max(data, function(d) { return d.values[yAttr]; })])
                     .range([config.plotHeight-config.margin.bottom-config.margin.top, 0]);
                 leftAxis.scale(y);
-                //leftAxisG.call(leftAxis);
 
                 // Tooltips
                 var tip = d3.tip()
                   .attr('class', 'd3-tip')
                   .offset([-10, 0])
-                  .html(function(d) { return '<span class="propertyName">' + d.yearRange + '</span>'; });
+                  .html(function(d) { return '<div class="propertyName">' + d.key +
+                      '</div><div class="propertyname">' + yAttr + ': ' +
+                      d.values[yAttr] + '</div>'; });
                 chart.call(tip);
 
                 // Bars
                 var bars = chart.selectAll('bar')
                     .data(data)
                     .enter().append('rect')
-                    .attr('transform', function(d) { return 'translate(' + (x(d.yearRange) + config.margin.left).toString() + ',0)'; })
-                    .attr('y', function(d) {return y(d[config.yLabel]) + config.margin.top; })
-                    .attr('height', function(d) { return config.plotHeight - y(d[config.yLabel]) - config.margin.top - config.margin.bottom; })
+                    .attr('transform', function(d) { return 'translate(' + (x(d.key) + config.margin.left).toString() + ',0)'; })
+                    .attr('y', function(d) {return y(d.values[yAttr]) + config.margin.top; })
+                    .attr('height', function(d) { return config.plotHeight - y(d.values[yAttr]) - config.margin.top - config.margin.bottom; })
                     .attr('width', x.rangeBand())
                     .on('mouseover', tip.show)
                     .on('mouseout', tip.hide);
 
+                // Animation
+                function refreshScale() {
+                    y.domain([0, d3.max(data, function(d) { return d.values[yAttr]; })]);
+                }
+
+                function refreshData() { // This thing doesn't work properly; complains of NaNs and I can't figure out where/how
+                    var transitionMillis = config.transitionMillis;
+                    refreshScale();
+                    chart.selectAll('rect')
+                        .transition()
+                        .ease('linear')
+                        .duration(transitionMillis)
+                        .attr('height', function(d) { return config.plotHeight - y(d.values[yAttr]) - config.margin.top - config.margin.bottom; });
+                }
+
+                $scope.selectedYChanged = function() {
+                    yAttr = $scope.selectedY;
+                    refreshData();
+                };
             };
         };
 
