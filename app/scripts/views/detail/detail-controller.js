f7dd759a1b83c05dab02f8ba8629d158a257c530
CloudNiner <andrew.fink.11@gmail.com>
2014-12-10 11:13:21 -0500

    Add detail filter dropdown

--- app/scripts/views/detail/detail-controller.js
+++ app/scripts/views/detail/detail-controller.js
@@ -13,6 +13,10 @@
             'fuel_oil': 'Oil Usage',
             'steam': 'Steam Usage',
             'water_use': 'Water Usage'
+        },
+        FILTER: {
+            NONE: 'none',
+            SECTOR: 'sector'
         }
     };
 
@@ -24,16 +28,34 @@
 /* jshint laxbreak:true */
         var building = buildingData.data && buildingData.data.rows && buildingData.data.rows.length > 0
                         ? buildingData.data.rows[0] : {};
-        var sectorColor = MOSColors[building.sector];
 /* jshint laxbreak:false */
+        var sectorColor = MOSColors[building.sector];
+        var rows = currentData.data.rows;
+        var selectedFilterText = {};
+        selectedFilterText[DetailConfig.FILTER.NONE] = 'All Buildings';
+        selectedFilterText[DetailConfig.FILTER.SECTOR] = 'Similar Building Types';
+
         $scope.building = building;
         $scope.fields = DetailConfig.fields;
-        $scope.currentData = currentData.data.rows;
+        $scope.currentData = rows;
         $scope.filterField = 'sector';
         $scope.calloutColor = sectorColor;
+        $scope.FILTER = DetailConfig.FILTER;
+        $scope.filterButtonText = selectedFilterText[DetailConfig.FILTER.NONE];
         $scope.headerColor = {
             'background-color': sectorColor,
             opacity: 0.8
+        };
+
+        $scope.filterBy = function (filter) {
+            if (filter === DetailConfig.FILTER.SECTOR) {
+                $scope.currentData = _.filter(rows, function (row) {
+                    return row.sector === building.sector;
+                });
+            } else {
+                $scope.currentData = rows;
+            }
+            $scope.filterButtonText = selectedFilterText[filter];
         };
     }
 
