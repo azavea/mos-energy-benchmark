6adbac0afee672178ede4db910b0f1e8f012ddd8
Nathan Zimmerman <npzimmerman@gmail.com>
2014-12-19 15:04:57 -0500

    Fix glossary and add it to compare/detail

--- app/scripts/views/charts/charts-glossary-directive.js
+++ app/scripts/views/charts/charts-glossary-directive.js
@@ -8,20 +8,27 @@
         // Begin directive module definition
         var module = {};
 
+        module.scope = {
+            page: '@'
+        };
+
         module.restrict = 'EA';
         module.templateUrl = 'scripts/views/charts/charts-glossary-partial.html',
 
         module.link = function ($scope, element, attrs) {
-            // Don't show the glossary on the initial panel
-            element.hide();
-
-            $scope.$on('header:on-not-top', function (event) {
-                element.show();
-            });
-
-            $scope.$on('header:on-top', function (event) {
+            var page = $scope.page;
+            if (page === 'charts') {
+                // Don't show the glossary on the initial panel
                 element.hide();
-            });
+
+                $scope.$on('header:on-not-top', function (event) {
+                    element.show();
+                });
+
+                $scope.$on('header:on-top', function (event) {
+                    element.hide();
+                });
+            }
         };
 
         return module;
