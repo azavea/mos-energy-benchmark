37f6ede6df4d92bdde1ea70daec77667176980a4
CloudNiner <andrew.fink.11@gmail.com>
2014-12-23 10:59:45 -0500

    Fixes #159: Callout tooltip for first slide button, unstyled

--- app/scripts/views/charts/charts-controller.js
+++ app/scripts/views/charts/charts-controller.js
@@ -1,15 +1,25 @@
 (function () {
     'use strict';
 
+    var COOKIE_STRING_SLIDE_CALLOUT = 'mos.charts.slideCallout.hide';
+
     /*
      * ngInject
      */
-    function ChartsController($scope, CartoSQLAPI, ChartingUtils, currentData, currentAllData, groupedData, previousData) {
+    function ChartsController($cookieStore, $scope, CartoSQLAPI, ChartingUtils, currentData, currentAllData, groupedData, previousData) {
         $scope.currentData = currentData.data.rows;
         $scope.currentAllData = currentAllData.data.rows;
         $scope.groupedData = groupedData.data.rows;
         $scope.combinedData = CartoSQLAPI.getCombinedData(currentData.data, previousData.data);
         $scope.seedData = ChartingUtils.seed(20);
+
+        $scope.hideCallout = $cookieStore.get(COOKIE_STRING_SLIDE_CALLOUT) || false;
+
+        $scope.calloutClicked = function () {
+            var hide = true;
+            $cookieStore.put(COOKIE_STRING_SLIDE_CALLOUT, hide);
+            $scope.hideCallout = hide;
+        };
     }
 
     angular.module('mos.views.charts')
