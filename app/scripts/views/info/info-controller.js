c9cfc9df69e7e262e80abe9219b3cc472f3f91bd
Kathryn Killebrew <kkillebrew@azavea.com>
2017-10-02 16:17:48 -0400

    Parameterize years on info page
    Get years from Carto service and use them on the info page.

--- app/scripts/views/info/info-controller.js
+++ app/scripts/views/info/info-controller.js
@@ -4,7 +4,26 @@
     /*
      * ngInject
      */
-    function InfoController() {
+    function InfoController($scope, CartoSQLAPI) {
+
+        $scope.yearsString = '';
+        setYearsString(CartoSQLAPI.years);
+
+        // Update years values and download links after they have been loaded from Carto
+        $scope.$on('mos.cartodb:years', function(event, data) {
+            setYearsString(data);
+        });
+
+        // Helper to build a pretty string of the years in the app, like "2013, 2014, and 2015"
+        function setYearsString(years) {
+            if (years && years.length > 1) {
+                // get years in ascending order without modifying original
+                var yrs = years.slice().reverse();
+                $scope.yearsString = [_.dropRight(yrs, 1).join(', '),
+                                      ', and ',
+                                      _.last(yrs)].join('');
+            }
+        }
     }
 
     angular.module('mos.views.info')
